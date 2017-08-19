ab9db8f61a7c471f5b302a95fc1224c916cf9598
dhanu93 <dhanu.anjana.93@gmail.com>
2017-08-19 17:13:35 +0530

    dashboard changed.

--- front_end/dashboard/app/main.js
+++ front_end/dashboard/app/main.js
@@ -1,14 +1,15 @@
 var app = angular.module('dashboard_main', []);
 app.controller('dashboard_main_controller', ['$scope', function ($scope) {
 
-    $scope.templateURL = 'startup.html';
+    //$scope.templateURL = 'teacher_dashboard.html';
+    $scope.templateURL = 'select_category.html';
 
     $scope.LoadDashboard = function(){
         $scope.templateURL = 'startup.html';
     };
 
     $scope.AddPapers = function(){
-        $scope.templateURL = 'add_paper.html';
+        $scope.templateURL = 'add_questions.html';
     };
 
 }]);
