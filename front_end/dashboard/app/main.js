a1ea06e0bc862a4ce4befa7a62dbf1055ea1e692
dhanu93 <dhanu.anjana.93@gmail.com>
2017-08-21 16:52:52 +0530

    question add modified

--- front_end/dashboard/app/main.js
+++ front_end/dashboard/app/main.js
@@ -1,15 +1,43 @@
 var app = angular.module('dashboard_main', []);
-app.controller('dashboard_main_controller', ['$scope', function ($scope) {
+app.controller('dashboard_main_controller', ['$scope', '$compile', function ($scope, $compile) {
 
     //$scope.templateURL = 'teacher_dashboard.html';
-    $scope.templateURL = 'select_category.html';
+    $scope.templateURL = 'add_questions.html';
+
+    $scope.newly_added_question_remover = false;
 
     $scope.LoadDashboard = function(){
-        $scope.templateURL = 'startup.html';
+    	$scope.templateURL = 'startup.html';
     };
 
     $scope.AddPapers = function(){
-        $scope.templateURL = 'add_questions.html';
+    	$scope.templateURL = 'add_questions.html';
     };
 
+    $scope.AddAnotherQuestionForm = function(){
+
+    	if(!$scope.newly_added_question_remover){
+    		$scope.newly_added_question_remover = true;
+    	}
+
+    	var myElements = angular.element(document.querySelector('#form-holder'));    	
+    	var html = angular.element(myElements[0].children[0]).clone();
+    	console.log(myElements);
+    	angular.element(myElements[0].children[myElements[0].children.length - 2]).after(html);
+    	$compile(html)($scope);
+    }
+
+    $scope.RemoveAddedQuestion = function(eve){
+    	var myElements = angular.element(eve.currentTarget);
+    	var parent = angular.element(myElements.parent())
+    	
+    	console.log(parent.length);
+
+    	if(parent.length <= 2){
+    		$scope.newly_added_question_remover = false;
+    	}
+
+    	parent.remove();
+    	$compile(parent);   	
+    }
 }]);
